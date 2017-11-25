dc21506edb9276f1f4ebdeb68e7aec2d88d0a940
Eric Tillberg <eric.tillberg@gmail.com>
2017-11-25 18:19:32 -0500

    Remove region_neighbors table in favor of a neighbors method on Region

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,11 +1,9 @@
 class Region < ApplicationRecord
-  has_and_belongs_to_many(
-    :regions,
-    :join_table => "region_neighbors",
-    :foreign_key => "region_a_id",
-    :association_foreign_key => "region_b_id"
-  )
   has_many :pieces
 
-  alias_attribute :neighbors, :regions
+  def neighbors
+    Settings.neighbors[self.name].map do |neighbor|
+      Region.find_by(name: neighbor)
+    end
+  end
 end
--- db/migrate/20171117041450_create_region_neighbors.rb
+++
@@ -1,8 +0,0 @@
-class CreateRegionNeighbors < ActiveRecord::Migration[5.1]
-  def change
-    create_table :region_neighbors, id: false do |t|
-      t.integer :region_a_id, null: false
-      t.integer :region_b_id, null: false
-    end
-  end
-end
