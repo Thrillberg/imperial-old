520c4b7d892b6666da469cf1e010f0f9cd169d90
Eric Tillberg <eric.tillberg@gmail.com>
2017-12-09 23:22:13 -0500

    svg of England and surrounding waters🌊

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pre_games#index'
+  get '/map', to: 'maps#show'
   resources :pre_games
   resources :games
   resources :pieces do
