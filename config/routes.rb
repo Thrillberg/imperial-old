5cc7971b8f10c5d9942f544ceeb839aa00570c4d
Eric Tillberg <eric.tillberg@gmail.com>
2018-02-02 10:44:24 -0500

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pre_games#index'
+
   post '/games/:game_id/investors/:id/build_factory', to: 'investors#build_factory'
   post '/games/:game_id/investors/:id/import', to: 'investors#import'
   post '/games/:game_id/investors/:id/maneuver', to: 'investors#maneuver'
   post '/games/:game_id/investors/:id/maneuver_destination', to: 'investors#maneuver_destination'
-  resources :boards
   resources :pre_games
   resources :games do
     resources :investors do
@@ -17,25 +17,6 @@ Rails.application.routes.draw do
         get :maneuver
         get :investor_turn
       end
-    end
-
-    member do
-      post :next_turn
-      get :production
-      get :build_factory
-      post :build_factory
-      get :import
-      post :import
-      post :maneuver
-      post :maneuver_destination
-      get :taxation
-      get :investor_turn
-      post :investor_turn
-    end
-  end
-  resources :pieces do
-    member do
-      put :move
     end
   end
 end
